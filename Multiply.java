ac0fa80141f4cf4e7f801f9737bf0cd6dad5cc78
rohit yadav <rohitkyadav82@gmail.com>
2022-11-11 12:31:33 +0530

    added Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -1,7 +1,7 @@
-public class Sum{
+public class Multiply {
     public static void main(String[] args) {
         int a=4;
         int b=3;
-        System.out.println("sum of number"+a+b);
+        System.out.println("sum of number"+(a+b));
     }
 }
